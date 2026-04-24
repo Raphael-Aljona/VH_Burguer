82ce843efa4b3fbbefb7734e57c82dc052e12494
Raphael-Aljona <raphael.aljona@gmail.com>
2026-04-24 10:02:26 -0300

    feat: Histórico de alterações do produto

---
+++ src/components/card-historico/card_historico.tsx
@@ -0,0 +1,15 @@
+import styles from "./card_historico.module.css"
+
+const CardHistorico = () => {
+    return(
+        <>
+            <div className={styles.card_historico}>
+                <p>12/12/12</p>
+                <p>Monstro</p>
+                <p>R$ 55,00</p>
+            </div>
+        </>
+    )
+}
+
+export default CardHistorico
---
+++ src/pages/historico/index.tsx
@@ -0,0 +1,30 @@
+import SubHeader from "@/components/sub-header/sub-header";
+import Footer from "@/components/footer/footer";
+import styles from "./historico.module.css"
+import CardHistorico from "@/components/card-historico/card_historico";
+
+const Historico = () => {
+    return (
+        <>
+            <main>
+                <SubHeader></SubHeader>
+                <section className={styles.secao_historico}>
+                    <div className={`layout_guide ${styles.container}`}>
+                        <h1>Histórico de alterações: Monstro</h1>
+                        <table >
+                            <tr className={styles.subtitulos}>
+                                <th>Data da alteração</th>
+                                <th>Nome anterior</th>
+                                <th>Preço Anterior</th>
+                            </tr>
+                        </table>
+                        <CardHistorico></CardHistorico>
+                    </div>
+                </section>
+                <Footer></Footer>
+            </main>
+        </>
+    )
+}
+
+export default Historico
